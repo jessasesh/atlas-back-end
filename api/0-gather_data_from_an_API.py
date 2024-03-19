df8ba7ec2268e7f300fe9de6978e618502f5ad57
jessasesh <jessica.dison@yahoo.com>
2024-03-19 23:43:53 +0000

    PEP correction

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,56 +3,34 @@
  Script that returns to-do list progress
  info about specified employee.
 """
-
 import requests
 import sys
 
 
-def emp_todo(employeeId):
+def emp_tasks(emp_id):
     """
-    Returns all tasks associated with an employee.
+    Employee's to do list
     """
-    url = "https://jsonplaceholder.typicode.com/"
-    url += "users/{}/todos".format(employeeId)
-    response = requests.get(url)
-    return response.json()
+    source = "https://jsonplaceholder.typicode.com"
+    user_url = f"{source}/users/{emp_id}"
+    todo_url = f"{source}/todos"
 
+    emp_data = requests.get(user_url).json()
+    todo_list = requests.get(todo_url, params={"userId": emp_id}).json()
 
-def emp_name(employeeId):
-    """
-    Returns employee name.
-    """
-    url = "https://jsonplaceholder.typicode.com/"
-    url += "users/{}".format(employeeId)
-    response = requests.get(url)
-    return response.json().get("name")
+    emp_name = emp_data.get("name")
+    done_tasks = []
+    for task in todo_list:
+        if task["completed"]:
+            done_tasks.append(task["title"])
 
+    all_len = len(todo_list)
+    done_len = len(done_tasks)
 
-def comp_tasks(tasks):
-    """
-    Returns number of completed tasks by specified employee.
-    """
-    completed_tasks = []
-
-    for task in tasks:
-        if task.get("completed"):
-            completed_tasks.append(task)
-    return completed_tasks
-
-
-def print_emp_tasks(employeeName, completedTasks, totalTasks):
-    """
-    Prints all tasks belonging to specified employee.
-    """
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, len(completedTasks), totalTasks))
-    for task in completedTasks:
-        print("\t {}".format(task.get("title")))
+    print(f"Employee {emp_name} is done with tasks({done_len}/{all_len}):")
+    for title in done_tasks:
+        print(f"\t {title}")
 
 
 if __name__ == "__main__":
-    employeeId = sys.argv[1]
-    tasks = emp_todo(employeeId)
-    employeeName = emp_name(employeeId)
-    completedTasks = comp_tasks(tasks)
-    print_emp_tasks(employeeName, completedTasks, len(tasks))
+    emp_tasks(int(sys.argv[1]))
