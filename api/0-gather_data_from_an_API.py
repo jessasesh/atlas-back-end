e3d3d6a5414f9d7ea209a6a92c1d4fc81d6c5f7c
jessasesh <jessica.dison@yahoo.com>
2024-03-19 23:58:33 +0000

    I hope this is it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,50 @@
 #!/usr/bin/python3
 """
- Script that returns to-do list progress
- info about specified employee.
+Python script that, for a given employee ID, returns
+information about his/her TODO list progress.
 """
+
 import requests
 import sys
 
 
-def emp_tasks(emp_id):
-    """
-    Employee's to do list
-    """
-    source = "https://jsonplaceholder.typicode.com"
-    user_url = f"{source}/users/{emp_id}"
-    todo_url = f"{source}/todos"
+def get_employee_tasks(employeeId):
+    """ Returns all tasks associated with an employee """
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}/todos".format(employeeId)
+    response = requests.get(url)
+    return response.json()
 
-    emp_data = requests.get(user_url).json()
-    todo_list = requests.get(todo_url, params={"userId": emp_id}).json()
 
-    emp_name = emp_data.get("name")
-    done_tasks = []
-    for task in todo_list:
-        if task["completed"]:
-            done_tasks.append(task["title"])
+def get_employee_name(employeeId):
+    """ Returns employee name from specific ID """
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}".format(employeeId)
+    response = requests.get(url)
+    return response.json().get("name")
 
-    all_len = len(todo_list)
-    done_len = len(done_tasks)
 
-    print(f"Employee {emp_name} is done with tasks({done_len}/{all_len}):")
-    for title in done_tasks:
-        print(f"\t {title}")
+def get_completed_tasks(tasks):
+    """ Returns all completed tasks """
+    completed_tasks = []
+
+    for task in tasks:
+        if task.get("completed"):
+            completed_tasks.append(task)
+    return completed_tasks
+
+
+def print_employee_tasks(employeeName, completedTasks, totalTasks):
+    """ Prints all tasks by employee ID """
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, len(completedTasks), totalTasks))
+    for task in completedTasks:
+        print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
-    emp_tasks(int(sys.argv[1]))
+    employeeId = sys.argv[1]
+    tasks = get_employee_tasks(employeeId)
+    employeeName = get_employee_name(employeeId)
+    completedTasks = get_completed_tasks(tasks)
+    print_employee_tasks(employeeName, completedTasks, len(tasks))
